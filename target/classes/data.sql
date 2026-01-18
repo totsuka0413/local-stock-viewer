b7b2f0b1611a0d177509255d9e373c1eb8c8a87e
angemessen <totsu_0128@yahoo.co.jp>
2026-01-18 17:40:48 +0900

    f

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO stock (symbol, code, name, category) VALUES ('3543.JP', '3543', 'コメダHD', 'HOLDING');
-INSERT INTO stock (symbol, code, name, category) VALUES ('2695.JP', '2695', 'くら寿司', 'HOLDING');
-INSERT INTO stock (symbol, code, name, category) VALUES ('4719.JP', '4719', 'アルファシステムズ', 'HOLDING');
-INSERT INTO stock (symbol, code, name, category) VALUES ('^SPX', NULL, 'S&P 500', 'WATCH');
